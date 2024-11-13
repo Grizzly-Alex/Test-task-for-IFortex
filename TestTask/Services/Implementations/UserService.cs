b2a6b5ae624079c363d18510b3e07a8d2e27b4a4
Alexander Medved <medved1991alexander@gmail.com>
2024-11-13 16:06:46 +0300

    implement GetUsers

--- TestTask/Services/Implementations/UserService.cs
+++ TestTask/Services/Implementations/UserService.cs
@@ -1,4 +1,6 @@
-﻿using TestTask.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
 using TestTask.Models;
 using TestTask.Services.Interfaces;
 
@@ -13,14 +15,27 @@ namespace TestTask.Services.Implementations
             _context = context;
         }
 
-        public Task<User> GetUser()
+        ///<returns>
+        /// Return the user with the maximum total amount of items delivered in 2003 or null
+        /// </returns>
+        public async Task<User?> GetUser()
         {
-            throw new NotImplementedException();
+            return await _context.Users
+                .OrderByDescending(user =>
+                    user.Orders.Where(order => order.CreatedAt.Year == 2003).Sum(o => o.Quantity))
+                .FirstOrDefaultAsync();
         }
 
-        public Task<List<User>> GetUsers()
+        ///<returns>
+        /// Return collection of users who have paid orders in 2010 or an empty collection
+        /// </returns>
+        public async Task<List<User>> GetUsers()
         {
-            throw new NotImplementedException();
+            return await _context.Users.Where(user =>
+                    user.Orders.Any(order => 
+                        order.Status == OrderStatus.Paid
+                        && order.CreatedAt.Year == 2010))
+                .ToListAsync();
         }
     }
 }
