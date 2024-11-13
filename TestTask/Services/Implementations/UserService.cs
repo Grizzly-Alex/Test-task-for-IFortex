d85f598584ad6cf0c8ac62b172601b115296e347
Alexander Medved <medved1991alexander@gmail.com>
2024-11-13 13:22:47 +0300

    implement GetUser

---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public sealed class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
