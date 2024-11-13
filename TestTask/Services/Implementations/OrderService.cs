6a4e6636d99280a318976ac19ec3c8ba0d66eb52
Alexander Medved <medved1991alexander@gmail.com>
2024-11-13 17:12:14 +0300

    implement GetOrder

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public sealed class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        ///<returns>
+        /// Return the most new order that contains more than one item or null
+        /// </returns>
+        public async Task<Order?> GetOrder()
+        {
+            return await _context.Orders.OrderByDescending(order => order.CreatedAt)
+                .FirstOrDefaultAsync(order => order.Quantity > 1);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
